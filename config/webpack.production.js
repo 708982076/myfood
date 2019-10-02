c44e81ceda294ca6e7c2d4d0c08b73d319b9d77c
chengpeng <chengpeng_1231@163.com>
2019-10-02 23:35:03 +0800

    1

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,11 +1,11 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 module.exports = {
   output: {
     filename: "scripts/[name].[hash:5].bundle.js",
-    publicPath: '/myfood/dist/'
+    publicPath: '/myfood/'
   },
   plugins: [
-    // new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin()
   ]
 }
